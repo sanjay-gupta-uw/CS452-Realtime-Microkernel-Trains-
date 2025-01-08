944d4e782e6869eb8651f249b265ff0d3568fa73
sanjay-gupta-uw <sanjayguppy@icloud.com>
2025-01-08 16:03:21 -0500

    testing upload

--- public/iotest/main.c
+++ public/iotest/main.c
@@ -1,23 +1,29 @@
 #include "rpi.h"
 
-int kmain() {
+int kmain()
+{
 	// set up GPIO pins for both console and marklin uarts
 	gpio_init();
 	// not strictly necessary, since console is configured during boot
 	uart_config_and_enable(CONSOLE);
 	// welcome message
-	uart_puts(CONSOLE, "\r\nHello world, this is version: " __DATE__ " / " __TIME__ "\r\n\r\nPress 'q' to reboot\r\n");
+	uart_puts(CONSOLE, "\r\nHello world, compiled by SG; this is version: " __DATE__ " / " __TIME__ "\r\n\r\nPress 'q' to reboot\r\n");
 
 	unsigned int counter = 1;
-	for (;;) {
+	for (;;)
+	{
 		uart_printf(CONSOLE, "PI[%u]> ", counter++);
-		for (;;) {
+		for (;;)
+		{
 			char c = uart_getc(CONSOLE);
 			uart_putc(CONSOLE, c);
-			if (c == '\r') {
+			if (c == '\r')
+			{
 				uart_putc(CONSOLE, '\n');
 				break;
-			} else if (c == 'q' || c == 'Q') {
+			}
+			else if (c == 'q' || c == 'Q')
+			{
 				uart_puts(CONSOLE, "\r\n");
 				return 0;
 			}
