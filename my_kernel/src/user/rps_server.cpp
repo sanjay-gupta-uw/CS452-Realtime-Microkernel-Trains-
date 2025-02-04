b8a1bb688290a1b0c234dd9e39e7370bd430376d
Jack Ma <y439ma@uwaterloo.ca>
2025-02-04 00:03:15 -0500

    Full RPS Server and Client implemented. Need to be fix.

--- my_kernel/src/user/rps_server.cpp
+++ my_kernel/src/user/rps_server.cpp
@@ -1,4 +1,5 @@
 #include "rps_server.h"
+#include "rps_client.h"
 #include "name_server.h"
 #include "../kern/syscall.h"
 #include "../rpi.h"
@@ -6,39 +7,22 @@
 
 #define MAX_PLAYERS 10
 
-enum RequestType {
-    SIGNUP,
-    PLAY,
-    QUIT
-};
-
-enum GameMove {
-    ROCK,
-    PAPER,
-    SCISSORS
-};
-
-struct GameRequest {
-    RequestType type;  
-    GameMove move;  
-};
-
 struct Player {
     int tid;
-    int choice;
-    bool active;
+    int choice = -1;
+    bool active = false;
+    bool inGame = false;
 };
 
 Player players[MAX_PLAYERS];
 int player_count = 0;
 
+void matchPlayers();
 void handlePlay(int index, int choice);
 void resolveGame(int p1_index, int p2_index);
 
 void RpsServer() {
     uart_printf(CONSOLE, "RPS Server running...\r\n");
-
-    // Register server name with the Name Server
     REGISTERAS("RPSServer");
 
     while (true) {
@@ -57,39 +41,73 @@ void RpsServer() {
         switch (req.type) {
             case SIGNUP:
                 if (index == -1 && player_count < MAX_PLAYERS) {
-                    players[player_count] = {sender_tid, -1, true};
+                    players[player_count] = {sender_tid, -1, true, false};
                     player_count++;
                     uart_printf(CONSOLE, "Player %d signed up.\r\n", sender_tid);
-                    REPLY(sender_tid, "OK", 3);  // Confirm signup
+                    matchPlayers(); // Check if two players can be paired
                 } else {
                     REPLY(sender_tid, "FULL", 5);  // Server full
                 }
                 break;
             case PLAY:
-                if (index != -1 && players[index].active) {
+                if (index != -1 && players[index].active && players[index].inGame) {
                     handlePlay(index, req.move);
                 }
                 break;
             case QUIT:
                 if (index != -1) {
                     players[index].active = false;
+                    players[index].inGame = false;
                     uart_printf(CONSOLE, "Player %d quit.\r\n", sender_tid);
-                    // Handle the case if player was in a game
-                    resolveGame(index, -1);
+                    resolveGame(index, -1); // Force resolve with no opponent
+                    REPLY(sender_tid, "QUIT_OK", 8);
                 }
                 break;
         }
-        YIELD(); 
+        YIELD();  // Yield CPU to other tasks
+    }
+}
+
+const char* getMoveName(int move) {
+    switch (move) {
+        case ROCK: return "Rock";
+        case PAPER: return "Paper";
+        case SCISSORS: return "Scissors";
+        default: return "Unknown";
+    }
+}
+
+const char* getResultDescription(int result) {
+    switch (result) {
+        case 0: return "Draw";
+        case 1: return "Wins";
+        case -1: return "Loses";
+        default: return "Error";
+    }
+}
+
+void matchPlayers() {
+    int first = -1;
+    for (int i = 0; i < player_count; ++i) {
+        if (players[i].active && !players[i].inGame && first == -1) {
+            first = i;
+        } else if (players[i].active && !players[i].inGame && first != -1) {
+            // Pair the players
+            players[first].inGame = players[i].inGame = true;
+            REPLY(players[first].tid, "PAIRED", 7);
+            REPLY(players[i].tid, "PAIRED", 7);
+            uart_printf(CONSOLE, "Players %d and %d are paired for a game.\r\n", players[first].tid, players[i].tid);
+            return;
+        }
     }
 }
 
 void handlePlay(int index, int choice) {
     players[index].choice = choice;
-    uart_printf(CONSOLE, "Player %d played choice %d.\r\n", players[index].tid, choice);
+    uart_printf(CONSOLE, "Player %d played %s.\r\n", players[index].tid, getMoveName(choice));
 
-    // Check for a match
     for (int i = 0; i < player_count; ++i) {
-        if (i != index && players[i].choice != -1 && players[i].active) {
+        if (i != index && players[i].choice != -1 && players[i].active && players[i].inGame) {
             resolveGame(index, i);
             break;
         }
@@ -103,27 +121,17 @@ void resolveGame(int p1_index, int p2_index) {
         return;
     }
 
-    int result1, result2;
-    if (players[p1_index].choice == players[p2_index].choice) {
-        result1 = result2 = 0; // Draw
-        uart_printf(CONSOLE, "Game draw between Player %d and Player %d.\r\n", players[p1_index].tid, players[p2_index].tid);
-    } else if ((players[p1_index].choice + 1) % 3 == players[p2_index].choice) {
-        result1 = -1; // p1 loses
-        result2 = 1;  // p2 wins
-        uart_printf(CONSOLE, "Player %d wins over Player %d.\r\n", players[p2_index].tid, players[p1_index].tid);
-    } else {
-        result1 = 1;  // p1 wins
-        result2 = -1; // p2 loses
-        uart_printf(CONSOLE, "Player %d wins over Player %d.\r\n", players[p1_index].tid, players[p2_index].tid);
-    }
+    const char* result1Desc = getResultDescription(players[p1_index].choice == players[p2_index].choice ? 0 :
+                                                   (players[p1_index].choice + 1) % 3 == players[p2_index].choice ? -1 : 1);
+    const char* result2Desc = getResultDescription(players[p1_index].choice == players[p2_index].choice ? 0 :
+                                                   (players[p1_index].choice + 1) % 3 == players[p2_index].choice ? 1 : -1);
 
-    uart_printf(CONSOLE, "Sending result to Player %d: %d\r\n", players[p1_index].tid, result1);
-    uart_printf(CONSOLE, "Sending result to Player %d: %d\r\n", players[p2_index].tid, result2);
+    uart_printf(CONSOLE, "Game result between Player %d and Player %d: %s, %s\r\n",
+                players[p1_index].tid, players[p2_index].tid, result1Desc, result2Desc);
 
-    REPLY(players[p1_index].tid, (char*)&result1, sizeof(result1));
-    REPLY(players[p2_index].tid, (char*)&result2, sizeof(result2));
+    REPLY(players[p1_index].tid, result1Desc, strlen(result1Desc) + 1);
+    REPLY(players[p2_index].tid, result2Desc, strlen(result2Desc) + 1);
 
-    // Reset choices after the game
     players[p1_index].choice = players[p2_index].choice = -1;
+    players[p1_index].inGame = players[p2_index].inGame = false;
 }
-
--- my_kernel/src/user/usertask.cpp
+++ my_kernel/src/user/usertask.cpp
@@ -6,41 +6,41 @@
 #include "rps_server.h"
 #include "rps_client.h"
 #include <cstring>
+void clientTask1() {
+    GameMove moves[] = {ROCK, PAPER, SCISSORS, QUIT};
+    RpsClient(moves, 4);
+}
 
-// Task1 starts the servers and clients
+void clientTask2() {
+    GameMove moves[] = {SCISSORS, ROCK, PAPER, PAPER, QUIT};
+    RpsClient(moves, 5);
+}
+
 void Task1() {
     uart_printf(CONSOLE, "FirstUserTask: Starting system services.\r\n");
 
-    // Start the Name Server
     int ns_tid = CREATE(HIGH, NameServer);
-
-    if (ns_tid < 0)
-    {
+    if (ns_tid < 0) {
         uart_printf(CONSOLE, "Failed to start Name Server. Exiting...\r\n");
         EXIT();
     }
     setNameServerTid(ns_tid);
     uart_printf(CONSOLE, "Name Server started with TID: %d\r\n", ns_tid);
 
-    // Start the RPS Server
     int rps_server_tid = CREATE(HIGH, RpsServer);
     uart_printf(CONSOLE, "RPS Server started with TID: %d\r\n", rps_server_tid);
 
-    // Delay to allow name registration to complete
-    YIELD();
-    YIELD();
+    YIELD(); 
+    YIELD(); // Allow servers to initialize
 
-    // Start RPS Clients
-    int client_tid1 = CREATE(MEDIUM, RpsClient);
-    int client_tid2 = CREATE(MEDIUM, RpsClient);
-    uart_printf(CONSOLE, "RPS Clients started with TIDs: %d, %d\r\n", client_tid1, client_tid2);
+    int client_tid1 = CREATE(MEDIUM, clientTask1);
+    int client_tid2 = CREATE(MEDIUM, clientTask2);
 
-    // Task1 now waits for some time then shuts down the system
+
     for (int i = 0; i < 20; ++i) {
-        YIELD();  // Allow other tasks to run
+        YIELD();
     }
 
-    // Shutdown message
     uart_printf(CONSOLE, "FirstUserTask: Shutting down system.\r\n");
-    EXIT();  // End Task1
+    EXIT();
 }
