878f07a3c2b73b0f3731b39b7178f56d3480b73c
Jack Ma <y439ma@uwaterloo.ca>
2025-03-21 17:23:25 -0400

    Adding track diagram and the updating logic

--- my_kernel/src/clock.d
+++
@@ -1,7 +0,0 @@
-src/clock.o: src/clock.cpp src/clock.h src/util.h src/rpi.h \
- src/user/include/io.h src/shared_constants.h
-src/clock.h:
-src/util.h:
-src/rpi.h:
-src/user/include/io.h:
-src/shared_constants.h:
--- my_kernel/src/rpi.d
+++
@@ -1,7 +0,0 @@
-src/rpi.o: src/rpi.cpp src/rpi.h src/util.h src/shared_constants.h \
- src/user/include/uassert.h src/user/include/../../include/syscall.h
-src/rpi.h:
-src/util.h:
-src/shared_constants.h:
-src/user/include/uassert.h:
-src/user/include/../../include/syscall.h:
